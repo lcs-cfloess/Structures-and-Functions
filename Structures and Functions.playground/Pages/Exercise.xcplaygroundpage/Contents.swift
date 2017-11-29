2e6855eade96f43c8262a7f56cce5c71b11a64f5
Floess, Constantin <cfloess@lcs-mba3343.lcs.on.ca>
2017-11-29 12:07:35 -0500

    progression on Big problem

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -32,4 +32,57 @@ import Foundation
 
 */
 
-// Begin here... 
+// Begin here...
+// our line is: y= -1/2x + 9.5
+// Our point is: (6|1.5)
+
+//Defining a structure representing a point on a Cartsain plane
+struct Point {
+    var x : Double = 0.0
+    var y : Double = 0.0
+}
+
+// crearing an instant of the poin in our problem
+var cabinsite = Point( x: 6, y: 1.5)
+
+
+// Definig a structure for slope
+struct Slope {
+    var rise : Double = 1.0
+    var run : Double = 1.0
+}
+
+//Defining a structure that repesants a line
+struct Line {
+    var slope : Slope = Slope(rise: 1.0, run: 1.0)
+    var verticalintercept = 0.0
+}
+
+
+// Create an instant of the line in our problem
+var slopeOfExistingRoade = Slope(rise: -1, run: 2)
+var existingRoade = Line(slope: slopeOfExistingRoade, verticalintercept: 9.5)
+
+
+/// Returns the slope of a lin Perpendicular to the givin line
+///
+/// - Parameter givenLine: The line we are staring rom
+/// - Returns: The slope of the line that is perpendicular
+func getSlopeOfPerpendicularLine(from givenLine: Line) -> Slope {
+    return Slope(rise: givenLine.slope.run, run: givenLine.slope.rise * -1)
+}
+
+//Get the Perpendicular slope
+let perpendicularSlope = getSlopeOfPerpendicularLine(from: existingRoade)
+
+
+func get VerticalIntersept(from p: Point, onLineWith m: Slope) -> Double {
+    // b = y - m * x
+    let mAsADecimal = m.rise / m.run
+}
+
+
+func distance(from: Point, to: Point) -> Double{
+    return sqrt(pow(from.x - to.x, 2) + pow(from.y - to.y, 2))
+}
+
